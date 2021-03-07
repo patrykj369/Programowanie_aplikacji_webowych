a40c2701f7f3d592e74ac5df41e10d28fc1bcc68
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-03-07 11:14:15 +0100

    do dokonczenia

---
+++ index2.ts
@@ -0,0 +1,30 @@
+class StatsApp{
+
+    data1Input: HTMLInputElement;
+    data2Input: HTMLInputElement;
+    data3Input: HTMLInputElement;
+    data4Input: HTMLInputElement;
+    dataSumaInput: HTMLInputElement;
+    dataAvgInput: HTMLInputElement;
+    dataMinInput: HTMLInputElement;
+    dataMaxInput: HTMLInputElement;
+
+    constructor(){
+        this.startApp();
+    }
+ 
+    startApp(){
+        this.getInputs;
+        this.watchInputValues();
+    }
+
+    getInputs(){
+        
+    }
+
+    watchInputValues(){
+        //this.data1Input.
+    }
+
+}
+
