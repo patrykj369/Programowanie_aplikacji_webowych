a70943e66db2d9f8319591c78054bb39dd839e02
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-03-21 23:13:12 +0100

    walka z odpowiednim czasem rozpoczęcia nagrywania

--- lab2/build/index.js
+++ lab2/build/index.js
@@ -1,4 +1,5 @@
 var channel1 = [];
+var start = 'stop';
 var clapAudio = document.querySelector('[data-sound="clap"]');
 var boomAudio = document.querySelector('[data-sound="boom"]');
 var hihatAudio = document.querySelector('[data-sound="hihat"]');
@@ -19,78 +20,84 @@ var tink = document.querySelector('#tink');
 var tom = document.querySelector('#tom');
 var playChannel1Btn = document.querySelector('#playChannel1');
 var startRecording1Btn = document.querySelector('#startRecording1');
+var stopRecording1Btn = document.querySelector('#stopRecording1');
+var removeRecording1Btn = document.querySelector('#removeRecording1');
 document.body.addEventListener('keypress', onKeyDown);
 document.body.addEventListener('transitionend', removeTransition);
 playChannel1Btn.addEventListener('click', onPlayChannel);
-// startRecording1Btn.addEventListener('click', startRecording);
-// function startRecording(): void{
-//     const key = klucz;
-//     const time = czas;
-//     channel1.push({
-//         key,
-//         time
-//     });
-// }
+startRecording1Btn.addEventListener('click', startRecording);
+stopRecording1Btn.addEventListener('click', stopRecording);
+removeRecording1Btn.addEventListener('click', removeRecording);
 function removeTransition(e) {
     if (e.propertyName !== 'transform')
         return;
     e.target.classList.remove('playing');
 }
 function onKeyDown(ev) {
-    var _a;
     var key = ev.key;
     var time = ev.timeStamp;
-    //console.log(key);
-    // channel1.push({
-    //     key,
-    //     time
-    // });
-    (_a = clap.classList) === null || _a === void 0 ? void 0 : _a.remove('playing');
+    if (start === 'start') {
+        channel1.push({
+            key: key,
+            time: time
+        });
+    }
+    else {
+        console.log("nagrywanie wylaczone");
+    }
     playSound(key);
-    //console.log(channel1);
 }
 function playSound(key) {
     switch (key) {
+        case 'A':
         case 'a':
             clapAudio.currentTime = 0;
             clap.classList.add('playing');
             clapAudio.play();
             break;
+        case 'S':
         case 's':
             boomAudio.currentTime = 0;
             boom.classList.add('playing');
             boomAudio.play();
             break;
+        case 'D':
         case 'd':
             hihatAudio.currentTime = 0;
             hihat.classList.add('playing');
             hihatAudio.play();
             break;
+        case 'F':
         case 'f':
             kickAudio.currentTime = 0;
             kick.classList.add('playing');
             kickAudio.play();
             break;
+        case 'G':
         case 'g':
             openhatAudio.currentTime = 0;
             openhat.classList.add('playing');
             openhatAudio.play();
             break;
+        case 'H':
         case 'h':
             rideAudio.currentTime = 0;
             ride.classList.add('playing');
             rideAudio.play();
             break;
+        case 'J':
         case 'j':
             snareAudio.currentTime = 0;
             snare.classList.add('playing');
             snareAudio.play();
             break;
+        case 'K':
         case 'k':
             tinkAudio.currentTime = 0;
             tink.classList.add('playing');
             tinkAudio.play();
             break;
+        case 'L':
         case 'l':
             tomAudio.currentTime = 0;
             tom.classList.add('playing');
@@ -98,8 +105,22 @@ function playSound(key) {
             break;
     }
 }
+function startRecording() {
+    //console.log(timeCurrent);
+    start = 'start';
+}
+function stopRecording() {
+    start = 'stop';
+}
+function removeRecording() {
+    while (channel1.length > 0) {
+        channel1.pop();
+    }
+    console.log(channel1);
+}
 function onPlayChannel() {
     playChannel();
+    console.log(channel1);
 }
 function playChannel() {
     var prevTime = 0;
--- lab2/src/index.ts
+++ lab2/src/index.ts
@@ -1,4 +1,5 @@
 const channel1: any[] = [];
+let start: string = 'stop';
 const clapAudio: HTMLAudioElement = document.querySelector('[data-sound="clap"]');
 const boomAudio: HTMLAudioElement = document.querySelector('[data-sound="boom"]');
 const hihatAudio: HTMLAudioElement = document.querySelector('[data-sound="hihat"]');
@@ -21,24 +22,18 @@ const tom: HTMLButtonElement = document.querySelector('#tom');
 
 const playChannel1Btn: HTMLButtonElement = document.querySelector('#playChannel1');
 const startRecording1Btn: HTMLButtonElement = document.querySelector('#startRecording1');
+const stopRecording1Btn: HTMLButtonElement = document.querySelector('#stopRecording1');
+const removeRecording1Btn: HTMLButtonElement = document.querySelector('#removeRecording1');
 
 document.body.addEventListener('keypress', onKeyDown);
 document.body.addEventListener('transitionend', removeTransition);
 playChannel1Btn.addEventListener('click', onPlayChannel);
-// startRecording1Btn.addEventListener('click', startRecording);
+startRecording1Btn.addEventListener('click', startRecording);
+stopRecording1Btn.addEventListener('click', stopRecording);
+removeRecording1Btn.addEventListener('click', removeRecording);
 
-// function startRecording(): void{
 
-//     const key = klucz;
-//     const time = czas;
-
-//     channel1.push({
-//         key,
-//         time
-//     });
-// }
-
-function removeTransition(e: any) {
+function removeTransition(e:any) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
 }
@@ -46,72 +41,99 @@ function removeTransition(e: any) {
 function onKeyDown(ev:KeyboardEvent): void{
     const key = ev.key;
     const time = ev.timeStamp;
+    if(start === 'start'){
 
-    //console.log(key);
-
-    // channel1.push({
-    //     key,
-    //     time
-    // });
+        channel1.push({
+            key,
+            time
+       });
+    }else{
+        console.log("nagrywanie wylaczone");
+    }
 
     playSound(key);
 
-    //console.log(channel1);
 }
 
 function playSound(key: string){
     switch(key){
+        case 'A':
         case 'a':
             clapAudio.currentTime = 0;
             clap.classList.add('playing');
             clapAudio.play();
             break;
+        case 'S':
         case 's':
             boomAudio.currentTime = 0;
             boom.classList.add('playing');
             boomAudio.play();
             break;
+        case 'D':
         case 'd':
             hihatAudio.currentTime = 0;
             hihat.classList.add('playing');
             hihatAudio.play();
             break;
+        case 'F':
         case 'f':
             kickAudio.currentTime = 0;
             kick.classList.add('playing');
             kickAudio.play();
             break;
+        case 'G':
         case 'g':
             openhatAudio.currentTime = 0;
             openhat.classList.add('playing');
             openhatAudio.play();
             break;
+        case 'H':
         case 'h':
             rideAudio.currentTime = 0;
             ride.classList.add('playing');
             rideAudio.play();
             break;
+        case 'J':
         case 'j':
             snareAudio.currentTime = 0;
             snare.classList.add('playing');
             snareAudio.play();
             break;
+        case 'K':
         case 'k':
             tinkAudio.currentTime = 0;
             tink.classList.add('playing');
             tinkAudio.play();
             break;
+        case 'L':
         case 'l':
             tomAudio.currentTime = 0;
             tom.classList.add('playing');
             tomAudio.play();
             break;
     }
+}
 
+function startRecording(): void{
+    //console.log(timeCurrent);
+    start = 'start';
+}
+
+function stopRecording(): void{
+    start = 'stop';
+}
+
+function removeRecording(): void{
+    while(channel1.length>0){
+        channel1.pop();
+    }
+    console.log(channel1);
 }
 
 function onPlayChannel(): void {
     playChannel();
+
+    console.log(channel1);
 }
 
 function playChannel(): void{
