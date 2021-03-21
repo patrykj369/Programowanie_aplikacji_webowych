93e7313c00233cbbcbc12a856487b887853b3316
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-03-21 20:43:39 +0100

    dodano animacje klawiszy

--- lab2/build/index.js
+++ lab2/build/index.js
@@ -8,17 +8,44 @@ var rideAudio = document.querySelector('[data-sound="ride"]');
 var snareAudio = document.querySelector('[data-sound="snare"]');
 var tinkAudio = document.querySelector('[data-sound="tink"]');
 var tomAudio = document.querySelector('[data-sound="tom"]');
+var clap = document.querySelector('#clap');
+var boom = document.querySelector('#boom');
+var hihat = document.querySelector('#hihat');
+var kick = document.querySelector('#kick');
+var openhat = document.querySelector('#openhat');
+var ride = document.querySelector('#ride');
+var snare = document.querySelector('#snare');
+var tink = document.querySelector('#tink');
+var tom = document.querySelector('#tom');
 var playChannel1Btn = document.querySelector('#playChannel1');
+var startRecording1Btn = document.querySelector('#startRecording1');
 document.body.addEventListener('keypress', onKeyDown);
+document.body.addEventListener('transitionend', removeTransition);
 playChannel1Btn.addEventListener('click', onPlayChannel);
+// startRecording1Btn.addEventListener('click', startRecording);
+// function startRecording(): void{
+//     const key = klucz;
+//     const time = czas;
+//     channel1.push({
+//         key,
+//         time
+//     });
+// }
+function removeTransition(e) {
+    if (e.propertyName !== 'transform')
+        return;
+    e.target.classList.remove('playing');
+}
 function onKeyDown(ev) {
+    var _a;
     var key = ev.key;
     var time = ev.timeStamp;
     //console.log(key);
-    channel1.push({
-        key: key,
-        time: time
-    });
+    // channel1.push({
+    //     key,
+    //     time
+    // });
+    (_a = clap.classList) === null || _a === void 0 ? void 0 : _a.remove('playing');
     playSound(key);
     //console.log(channel1);
 }
@@ -26,38 +53,47 @@ function playSound(key) {
     switch (key) {
         case 'a':
             clapAudio.currentTime = 0;
+            clap.classList.add('playing');
             clapAudio.play();
             break;
         case 's':
             boomAudio.currentTime = 0;
+            boom.classList.add('playing');
             boomAudio.play();
             break;
         case 'd':
             hihatAudio.currentTime = 0;
+            hihat.classList.add('playing');
             hihatAudio.play();
             break;
         case 'f':
             kickAudio.currentTime = 0;
+            kick.classList.add('playing');
             kickAudio.play();
             break;
         case 'g':
             openhatAudio.currentTime = 0;
+            openhat.classList.add('playing');
             openhatAudio.play();
             break;
         case 'h':
             rideAudio.currentTime = 0;
+            ride.classList.add('playing');
             rideAudio.play();
             break;
         case 'j':
             snareAudio.currentTime = 0;
+            snare.classList.add('playing');
             snareAudio.play();
             break;
         case 'k':
             tinkAudio.currentTime = 0;
+            tink.classList.add('playing');
             tinkAudio.play();
             break;
         case 'l':
             tomAudio.currentTime = 0;
+            tom.classList.add('playing');
             tomAudio.play();
             break;
     }
--- lab2/src/index.ts
+++ lab2/src/index.ts
@@ -9,10 +9,39 @@ const snareAudio: HTMLAudioElement = document.querySelector('[data-sound="snare"
 const tinkAudio: HTMLAudioElement = document.querySelector('[data-sound="tink"]');
 const tomAudio: HTMLAudioElement = document.querySelector('[data-sound="tom"]');
 
+const clap: HTMLButtonElement = document.querySelector('#clap');
+const boom: HTMLButtonElement = document.querySelector('#boom');
+const hihat: HTMLButtonElement = document.querySelector('#hihat');
+const kick: HTMLButtonElement = document.querySelector('#kick');
+const openhat: HTMLButtonElement = document.querySelector('#openhat');
+const ride: HTMLButtonElement = document.querySelector('#ride');
+const snare: HTMLButtonElement = document.querySelector('#snare');
+const tink: HTMLButtonElement = document.querySelector('#tink');
+const tom: HTMLButtonElement = document.querySelector('#tom');
+
 const playChannel1Btn: HTMLButtonElement = document.querySelector('#playChannel1');
+const startRecording1Btn: HTMLButtonElement = document.querySelector('#startRecording1');
 
 document.body.addEventListener('keypress', onKeyDown);
+document.body.addEventListener('transitionend', removeTransition);
 playChannel1Btn.addEventListener('click', onPlayChannel);
+// startRecording1Btn.addEventListener('click', startRecording);
+
+// function startRecording(): void{
+
+//     const key = klucz;
+//     const time = czas;
+
+//     channel1.push({
+//         key,
+//         time
+//     });
+// }
+
+function removeTransition(e: any) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
 
 function onKeyDown(ev:KeyboardEvent): void{
     const key = ev.key;
@@ -20,12 +49,13 @@ function onKeyDown(ev:KeyboardEvent): void{
 
     //console.log(key);
 
-    channel1.push({
-        key,
-        time
-    });
+    // channel1.push({
+    //     key,
+    //     time
+    // });
 
     playSound(key);
+
     //console.log(channel1);
 }
 
@@ -33,38 +63,47 @@ function playSound(key: string){
     switch(key){
         case 'a':
             clapAudio.currentTime = 0;
+            clap.classList.add('playing');
             clapAudio.play();
             break;
         case 's':
             boomAudio.currentTime = 0;
+            boom.classList.add('playing');
             boomAudio.play();
             break;
         case 'd':
             hihatAudio.currentTime = 0;
+            hihat.classList.add('playing');
             hihatAudio.play();
             break;
         case 'f':
             kickAudio.currentTime = 0;
+            kick.classList.add('playing');
             kickAudio.play();
             break;
         case 'g':
             openhatAudio.currentTime = 0;
+            openhat.classList.add('playing');
             openhatAudio.play();
             break;
         case 'h':
             rideAudio.currentTime = 0;
+            ride.classList.add('playing');
             rideAudio.play();
             break;
         case 'j':
             snareAudio.currentTime = 0;
+            snare.classList.add('playing');
             snareAudio.play();
             break;
         case 'k':
             tinkAudio.currentTime = 0;
+            tink.classList.add('playing');
             tinkAudio.play();
             break;
         case 'l':
             tomAudio.currentTime = 0;
+            tom.classList.add('playing');
             tomAudio.play();
             break;
     }
