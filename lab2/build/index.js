462408a5b90a3179368d479b23ce711657572339
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-03-22 23:52:45 +0100

    około 70% sukcesu

--- lab2/build/index.js
+++ lab2/build/index.js
@@ -1,5 +1,8 @@
 var channel1 = [];
+var channel2 = [];
 var start = 'stop';
+var timeC = 0;
+var currentChannel;
 var clapAudio = document.querySelector('[data-sound="clap"]');
 var boomAudio = document.querySelector('[data-sound="boom"]');
 var hihatAudio = document.querySelector('[data-sound="hihat"]');
@@ -18,33 +21,56 @@ var ride = document.querySelector('#ride');
 var snare = document.querySelector('#snare');
 var tink = document.querySelector('#tink');
 var tom = document.querySelector('#tom');
+var myBar = document.querySelector('#myBar');
 var playChannel1Btn = document.querySelector('#playChannel1');
+var playChannel2Btn = document.querySelector('#playChannel2');
 var startRecording1Btn = document.querySelector('#startRecording1');
+var startRecording2Btn = document.querySelector('#startRecording2');
 var stopRecording1Btn = document.querySelector('#stopRecording1');
+var stopRecording2Btn = document.querySelector('#stopRecording2');
 var removeRecording1Btn = document.querySelector('#removeRecording1');
 document.body.addEventListener('keypress', onKeyDown);
 document.body.addEventListener('transitionend', removeTransition);
+//myBar.addEventListener('transitioned', removePress);
 playChannel1Btn.addEventListener('click', onPlayChannel);
+playChannel2Btn.addEventListener('click', onPlayChannel);
 startRecording1Btn.addEventListener('click', startRecording);
+startRecording2Btn.addEventListener('click', startRecording);
 stopRecording1Btn.addEventListener('click', stopRecording);
-removeRecording1Btn.addEventListener('click', removeRecording);
+stopRecording2Btn.addEventListener('click', stopRecording);
 function removeTransition(e) {
     if (e.propertyName !== 'transform')
         return;
     e.target.classList.remove('playing');
 }
+// function removePress(e:any) {
+//     if (e.propertyName !== 'width') return;
+//     if (myBar.style.width == 100+"%"){
+//         playChannel1Btn.classList.remove('keyRecordPress');
+//         playChannel2Btn.classList.remove('keyRecordPress');
+//     }
+// }
 function onKeyDown(ev) {
     var key = ev.key;
-    var time = ev.timeStamp;
-    if (start === 'start') {
-        channel1.push({
-            key: key,
-            time: time
-        });
+    var time = ev.timeStamp - timeC;
+    switch (currentChannel) {
+        case "startRecording1":
+            if (start === 'start')
+                channel1.push({
+                    key: key,
+                    time: time
+                });
+            break;
+        case "startRecording2":
+            if (start === 'start')
+                channel2.push({
+                    key: key,
+                    time: time
+                });
+            break;
     }
-    else {
-        console.log("nagrywanie wylaczone");
-    }
+    //console.log("Kanał2 " + channel2);
+    //console.log("Kanał1 " + channel1);
     playSound(key);
 }
 function playSound(key) {
@@ -105,27 +131,96 @@ function playSound(key) {
             break;
     }
 }
-function startRecording() {
+function startRecording(event) {
     //console.log(timeCurrent);
+    clearProgressBar();
+    currentChannel = event.target.id;
     start = 'start';
+    timeC = event.timeStamp;
+    switch (currentChannel) {
+        case "startRecording1":
+            startRecording1Btn.classList.add('keyRecordPress');
+            while (channel1.length > 0) {
+                channel1.pop();
+            }
+            ;
+            break;
+        case "startRecording2":
+            startRecording2Btn.classList.add('keyRecordPress');
+            while (channel2.length > 0) {
+                channel2.pop();
+            }
+            ;
+    }
 }
 function stopRecording() {
+    var _a, _b;
     start = 'stop';
+    (_a = startRecording1Btn.classList) === null || _a === void 0 ? void 0 : _a.remove('keyRecordPress');
+    (_b = startRecording2Btn.classList) === null || _b === void 0 ? void 0 : _b.remove('keyRecordPress');
 }
-function removeRecording() {
-    while (channel1.length > 0) {
-        channel1.pop();
-    }
-    console.log(channel1);
+function onPlayChannel(event) {
+    playChannel(event.target.id);
 }
-function onPlayChannel() {
-    playChannel();
-    console.log(channel1);
-}
-function playChannel() {
+function playChannel(channel) {
+    var _a, _b;
     var prevTime = 0;
-    channel1.forEach(function (sound) {
-        var timeout = sound.time - prevTime;
-        setTimeout(function () { return playSound(sound.key); }, timeout);
-    });
+    var lengtMusic1 = channel1.length;
+    var timeMusic1 = 0;
+    if (lengtMusic1 > 0)
+        timeMusic1 = channel1[lengtMusic1 - 1].time;
+    var lengtMusic2 = channel2.length;
+    var timeMusic2 = 0;
+    if (lengtMusic2 > 0)
+        timeMusic2 = channel2[lengtMusic2 - 1].time;
+    switch (channel) {
+        case "playChannel1":
+            playChannel1Btn.classList.add('keyRecordPress');
+            progressBar();
+            (_a = startRecording1Btn.classList) === null || _a === void 0 ? void 0 : _a.remove('keyRecordPress');
+            channel1.forEach(function (sound) {
+                var timeout = sound.time - prevTime;
+                setTimeout(function () { return playSound(sound.key); }, timeout);
+                setTimeout(function () { return playChannel1Btn.classList.remove('keyRecordPress'); }, timeMusic1);
+            });
+            break;
+        case "playChannel2":
+            playChannel2Btn.classList.add('keyRecordPress');
+            progressBar();
+            (_b = startRecording2Btn.classList) === null || _b === void 0 ? void 0 : _b.remove('keyRecordPress');
+            channel2.forEach(function (sound) {
+                var timeout = sound.time - prevTime;
+                setTimeout(function () { return playSound(sound.key); }, timeout);
+                setTimeout(function () { return playChannel2Btn.classList.remove('keyRecordPress'); }, timeMusic2);
+            });
+            break;
+    }
+}
+function progressBar() {
+    var lengtMusic = channel1.length;
+    var timeMusic = channel1[lengtMusic - 1].time;
+    var i = 0;
+    function move() {
+        if (i == 0) {
+            i = 1;
+            var elem_1 = document.getElementById("myBar");
+            var width_1 = 1;
+            var id_1 = setInterval(frame, timeMusic / 95);
+            function frame() {
+                if (width_1 >= 100) {
+                    clearInterval(id_1);
+                    i = 0;
+                }
+                else {
+                    width_1++;
+                    elem_1.style.width = width_1 + "%";
+                }
+            }
+        }
+    }
+    move();
+}
+function clearProgressBar() {
+    var elem = document.getElementById("myBar");
+    elem.style.width = 1 + "%";
 }
--- lab2/src/index.ts
+++ lab2/src/index.ts
@@ -1,5 +1,9 @@
 const channel1: any[] = [];
+const channel2: any[] = [];
 let start: string = 'stop';
+let timeC: number = 0;
+let currentChannel: string;
+
 const clapAudio: HTMLAudioElement = document.querySelector('[data-sound="clap"]');
 const boomAudio: HTMLAudioElement = document.querySelector('[data-sound="boom"]');
 const hihatAudio: HTMLAudioElement = document.querySelector('[data-sound="hihat"]');
@@ -20,37 +24,70 @@ const snare: HTMLButtonElement = document.querySelector('#snare');
 const tink: HTMLButtonElement = document.querySelector('#tink');
 const tom: HTMLButtonElement = document.querySelector('#tom');
 
+const myBar: HTMLDivElement = document.querySelector('#myBar');
+
 const playChannel1Btn: HTMLButtonElement = document.querySelector('#playChannel1');
+const playChannel2Btn: HTMLButtonElement = document.querySelector('#playChannel2');
+
 const startRecording1Btn: HTMLButtonElement = document.querySelector('#startRecording1');
+const startRecording2Btn: HTMLButtonElement = document.querySelector('#startRecording2');
+
 const stopRecording1Btn: HTMLButtonElement = document.querySelector('#stopRecording1');
+const stopRecording2Btn: HTMLButtonElement = document.querySelector('#stopRecording2');
+
 const removeRecording1Btn: HTMLButtonElement = document.querySelector('#removeRecording1');
 
 document.body.addEventListener('keypress', onKeyDown);
 document.body.addEventListener('transitionend', removeTransition);
-playChannel1Btn.addEventListener('click', onPlayChannel);
-startRecording1Btn.addEventListener('click', startRecording);
-stopRecording1Btn.addEventListener('click', stopRecording);
-removeRecording1Btn.addEventListener('click', removeRecording);
 
+//myBar.addEventListener('transitioned', removePress);
+
+playChannel1Btn.addEventListener('click', onPlayChannel);
+playChannel2Btn.addEventListener('click', onPlayChannel);
+
+startRecording1Btn.addEventListener('click', startRecording);
+startRecording2Btn.addEventListener('click', startRecording);
+
+stopRecording1Btn.addEventListener('click', stopRecording);
+stopRecording2Btn.addEventListener('click', stopRecording);
 
 function removeTransition(e:any) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
 }
 
+// function removePress(e:any) {
+//     if (e.propertyName !== 'width') return;
+//     if (myBar.style.width == 100+"%"){
+//         playChannel1Btn.classList.remove('keyRecordPress');
+//         playChannel2Btn.classList.remove('keyRecordPress');
+//     }
+// }
+
 function onKeyDown(ev:KeyboardEvent): void{
     const key = ev.key;
-    const time = ev.timeStamp;
-    if(start === 'start'){
+    const time = ev.timeStamp - timeC;
 
-        channel1.push({
-            key,
-            time
-       });
-    }else{
-        console.log("nagrywanie wylaczone");
-    }
 
+        switch(currentChannel){
+            case "startRecording1":
+            if(start === 'start')
+            channel1.push({
+                key,
+                time
+           });
+           break;
+           case "startRecording2":
+            if(start === 'start')
+            channel2.push({
+                key,
+                time
+           });
+           break;
+        }
+
+    //console.log("Kanał2 " + channel2);
+    //console.log("Kanał1 " + channel1);
     playSound(key);
 
 }
@@ -114,34 +151,108 @@ function playSound(key: string){
     }
 }
 
-function startRecording(): void{
+function startRecording(event): void{
     //console.log(timeCurrent);
+    clearProgressBar();
+    currentChannel = event.target.id;
     start = 'start';
+    timeC = event.timeStamp;
+    switch(currentChannel){
+        case "startRecording1":
+            startRecording1Btn.classList.add('keyRecordPress')
+            while(channel1.length>0){
+                channel1.pop();
+            };
+        break;
+        case "startRecording2":
+            startRecording2Btn.classList.add('keyRecordPress')
+            while(channel2.length>0){
+                channel2.pop();
+            };
+    }
+
 }
 
 function stopRecording(): void{
     start = 'stop';
+    startRecording1Btn.classList?.remove('keyRecordPress');
+    startRecording2Btn.classList?.remove('keyRecordPress');
 }
 
-function removeRecording(): void{
-    while(channel1.length>0){
-        channel1.pop();
-    }
-    console.log(channel1);
+function onPlayChannel(event): void {
+    playChannel(event.target.id);
 }
 
-function onPlayChannel(): void {
-    playChannel();
-
-    console.log(channel1);
-}
-
-function playChannel(): void{
+function playChannel(channel: string): void{
     let prevTime = 0;
-    channel1.forEach(sound => {
-        const timeout = sound.time - prevTime;
-        setTimeout(()=> playSound(sound.key), timeout);
-    });
+    const lengtMusic1 = channel1.length;
+    let timeMusic1 = 0;
+    if(lengtMusic1>0)
+    timeMusic1 = channel1[lengtMusic1-1].time;
+
+    const lengtMusic2 = channel2.length;
+    let timeMusic2 = 0;
+    if(lengtMusic2>0)
+    timeMusic2 = channel2[lengtMusic2-1].time;
+
+    switch(channel){
+        case "playChannel1":
+            playChannel1Btn.classList.add('keyRecordPress');
+            progressBar();
+            startRecording1Btn.classList?.remove('keyRecordPress');
+
+            channel1.forEach(sound => {
+                const timeout = sound.time - prevTime;
+                setTimeout(()=> playSound(sound.key), timeout);
+                setTimeout(()=> playChannel1Btn.classList.remove('keyRecordPress'), timeMusic1);
+            });
+
+        break;
+        case "playChannel2":
+            playChannel2Btn.classList.add('keyRecordPress');
+            progressBar();
+            startRecording2Btn.classList?.remove('keyRecordPress');
+
+            channel2.forEach(sound => {
+            const timeout = sound.time - prevTime;
+            setTimeout(()=> playSound(sound.key), timeout);
+            setTimeout(()=> playChannel2Btn.classList.remove('keyRecordPress'), timeMusic2);
+        });
+
+        break;
+    }
+
 }
 
+function progressBar(): void{
+    const lengtMusic = channel1.length;
+    const timeMusic = channel1[lengtMusic-1].time;
+    let i = 0;
+    function move() {
+    if (i == 0) {
+    i = 1;
+    const elem = document.getElementById("myBar");
+    let width = 1;
+    let id = setInterval(frame, timeMusic/95);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+     }
+    }
+  }
+  move();
 
+
+}
+
+function clearProgressBar(): void{
+
+    var elem = document.getElementById("myBar");
+    elem.style.width = 1 + "%";
+
+}
